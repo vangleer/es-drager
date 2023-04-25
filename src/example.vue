e8c7a18100f35f7617a0c076c539dd27b8ff11c2
huangwantong <huangwantong@escopetech.com>
2023-04-25 14:07:45 +0800

    build

--- src/example.vue
+++ src/example.vue
@@ -13,7 +13,9 @@
 
 <script setup lang='ts'>
 import { ref } from 'vue'
-import Drager from './drager'
+// import Drager from './drager'
+import 'es-drager/lib/style.css'
+import Drager from 'es-drager'
 
 const dragList = ref([
   { text: '移动', zoomable: false },
