d8ec972429f4a1a72c71f03212ad21241cd98033
huangwantong <huangwantong@escopetech.com>
2023-04-25 14:25:31 +0800

    chore: ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()]
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    base: mode === 'build' ? '/es-drager' : ''
+  }
 })
