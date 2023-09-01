7f581431580c6e1198a9517ae4917b0d5f70089a
huangwantong <huangwantong@escopetech.com>
2023-09-01 11:21:52 +0800

    chore: 添加示例国际化

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,10 +1,8 @@
 <template>
-  <el-config-provider :locale="en">
+  <el-config-provider>
     <RouterView></RouterView>
   </el-config-provider>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import en from 'element-plus/dist/locale/en.mjs'
 </script>
